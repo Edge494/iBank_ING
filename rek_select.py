8fd4475bb6fc1e971e47446f818e79056b4e2173
edge494 <e.vanderplas@gmail.com>
2013-11-16 11:27:13 +0100

    First version of python script to rearrange ING bank statements for easy use with iBank

--- rek_select.py
+++ rek_select.py
@@ -1,12 +1,13 @@
 __author__ = 'Edmond van der Plas'
 __date__ = '16-11-2013'
-__version__ = '0.1.1'
-
+__version__ = '0.2'
 
 import pandas as pd
 
-#Importeren van de csv data
-df = pd.read_csv("/Users/Edge/Documents/IT/Python/test.csv", usecols=[0,1,3,5,6])
+import sys
+
+#Importeren van de csv data waarbij de filename van het csv bestand ingegeven is als argument in het starten van het python script
+df = pd.read_csv(sys.argv[1], usecols=[0,1,3,5,6,8])
 
 #Hernoemen van 2 kolommen voor makkelijker gebruik
 df.rename(columns={'Af Bij': 'afbij'}, inplace=True)
@@ -29,7 +30,7 @@ df['Bedrag2'] = df.apply(lambda row: (row['Bedrag']*row['C']
                                             else -row['Bedrag']*row['C']), axis=1)
 
 #Kolommen selecteren voor output
-df2 = df[['Datum','Omschrijving', 'Bedrag2','Tegenrekening']]
+df2 = df[['Datum','Omschrijving', 'Bedrag2','Tegenrekening','Mededelingen']]
 
 #Datastructuur naar csv schrijven
 df2.to_csv('test_result.csv', sep=',', na_rep='0', dtype=int)
